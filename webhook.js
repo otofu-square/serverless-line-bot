12cf72354beb122f61676e077192672e2ef42fb5
otofu-square <yukita@feedforce.jp>
2016-12-04 19:36:20 +0900

    Add ping

--- webhook.js
+++ webhook.js
@@ -1,13 +1,27 @@
 'use strict';
 
-module.exports.main = (event, context, callback) => {
-  const response = {
+const LineMessagingAPI = require('./lib/line_messaging_api');
+const client = new LineMessagingAPI(process.env.lineChannelAccessToken);
+
+function createResponse(event) {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      body: event.body,
     }),
   };
+}
+
+module.exports.main = (event, context, callback) => {
+  const lineEvent = JSON.parse(event.body).events[0];
+
+  if (lineEvent.message.text === 'ping') {
+    client.reply(lineEvent.replyToken, 'pong');
+  }
+
+  const response = createResponse(event);
+  console.log(response);
 
   callback(null, response);
-};
+}
