4177fbb695e1a96fa4231b07577f650bf4538520
otofu-square <yukita@feedforce.jp>
2016-12-05 21:37:06 +0900

    Add create

--- webhook.js
+++ webhook.js
@@ -2,6 +2,7 @@
 
 const all = require('./lib/all');
 const ping = require('./lib/ping');
+const create = require('./lib/create');
 const deleteBySubID = require('./lib/delete');
 
 function createResponse(event) {
@@ -14,15 +15,26 @@ function createResponse(event) {
   };
 }
 
+function getFrom(source) {
+  if (source.groupId) return source.groupId;
+  if (source.roomId) return source.roomId;
+  if (source.userId) return source.userId;
+  return null;
+}
+
 module.exports.main = (event, context, callback) => {
   const lineEvent = JSON.parse(event.body).events[0];
   const replyToken = lineEvent.replyToken;
   const text = lineEvent.message.text;
+  const from = getFrom(lineEvent.source);
 
   if (text === 'ping') ping(replyToken);
   if (text === 'reminder all') all(replyToken);
   if (text.match(/^reminder delete /)) {
     deleteBySubID(replyToken, text.match(/^reminder delete (.*)/)[1]);
+  }
+  if (text.match(/^reminder add /)) {
+    create(replyToken, from, text.match(/^reminder add (.*)/)[1]);
   }
 
   const response = createResponse(event);
